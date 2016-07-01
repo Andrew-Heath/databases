bbfbb471e99ee3d14ac0cf58b7ccc57b7e2c77a0
Andrew-Heath <aheath42@gmail.com>
2016-07-01 16:47:23 -0700

    Create database and tables; organize test runner

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,30 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(22) NOT NULL,
+  PRIMARY KEY(id)
+
 );
+
+CREATE TABLE rooms(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(22) NOT NULL,
+  PRIMARY KEY(id)
+
+);
+
+CREATE TABLE messages (
+  id INT NOT NULL AUTO_INCREMENT,
+  message VARCHAR(255),
+  user INT NOT NULL,
+  room INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(user) REFERENCES users(id),
+  FOREIGN KEY(room) REFERENCES rooms(id)
+);
+
 
 /* Create other tables and define schemas for them here! */
 
